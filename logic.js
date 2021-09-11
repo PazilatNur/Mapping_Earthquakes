b89dac886c38fd4accfca353117a43c64d6b7724
PazilatNur <79488124+PazilatNur@users.noreply.github.com>
2021-09-11 12:59:12 -0400

    multipoints

--- logic.js
+++ logic.js
@@ -2,16 +2,38 @@ console.log("working");
 
 let map = L.map('mapid').setView([40.7, -94.5], 5);
 
+// cities.forEach(city=>{
+//   console.log(city);
+//   let marker=L.marker(city.location)
+//   .bindPopup("<h2>"+city.city +","+ city.state + "</h2><hr><h3> Population: " + city.population.toLocaleString()  + "</h3>" ).addTo(map);
+// })
+
+
+cities.forEach(city=>{
+  console.log(city);
+  L.circleMarker(city.location ,{
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>"+city.city +","+ city.state + "</h2><hr><h3> Population: " + city.population.toLocaleString()  + "</h3>" ).addTo(map);
+})
+
+
+
+
+
 // let marker=L.marker([34.0522, -118.2437]).addTo(map);
 // L.circleMarker([34.0522, -118.2437], {
 //   radius:100});
 
-L.circleMarker([34.0522, -118.2437], {
-  radius: 100,
-  color: "red",
-  fillColor:"#ffffa1"
-}).addTo(map);
+// L.circleMarker([34.0522, -118.2437], {
+//   radius: 100,
+//   color: "red",
+//   fillColor:"#ffffa1"
+// }).addTo(map);
 // api.mapbox.com/styles/v1/{id}/tiles
+
+
+
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
